8e88eab5bcd90c0ef434375751b3a12d26978445
Vipransh Sharma <vipransh0228@gmail.com>
2026-02-08 11:37:00 +0530

    Add files via upload

---
+++ q8.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int power(int a, int b) {
+    // Base case
+    if (b == 0)
+        return 1;
+
+    // Recursive case
+    return a * power(a, b - 1);
+}
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    printf("%d", power(a, b));
+    return 0;
+}
